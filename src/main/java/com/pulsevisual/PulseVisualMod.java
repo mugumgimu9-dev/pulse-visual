93dd7b36a413f270fb98bdec54dfa77149d8ff8a
mugumgimu9-dev <mugumgimu9@gmail.com>
2026-06-05 16:03:01 +0300

    Update PulseVisualMod.java

--- src/main/java/com/pulsevisual/PulseVisualMod.java
+++ src/main/java/com/pulsevisual/PulseVisualMod.java
@@ -5,6 +5,6 @@ import net.fabricmc.api.ModInitializer;
 public class PulseVisualMod implements ModInitializer {
     @Override
     public void onInitialize() {
-        System.out.println("[Pulse Visual] Common initialization complete!");
+        System.out.println("Pulse Visual Mod Initialized!");
     }
 }
