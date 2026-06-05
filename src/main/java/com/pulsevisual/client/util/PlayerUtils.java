f0d6c492703d0d1c0b7869ae98e47b4d4a7217ac
mugumgimu9-dev <mugumgimu9@gmail.com>
2026-06-05 03:41:36 +0300

    Update PlayerUtils.java

--- src/main/java/com/pulsevisual/client/util/PlayerUtils.java
+++ src/main/java/com/pulsevisual/client/util/PlayerUtils.java
@@ -1,42 +1,17 @@
 package com.pulsevisual.client.util;
 
-import net.fabricmc.api.Environment;
-import net.fabricmc.api.EnvType;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.math.Vec3i;
 
-@Environment(EnvType.CLIENT)
 public class PlayerUtils {
     private static final MinecraftClient client = MinecraftClient.getInstance();
 
-    public static Vec3d getPlayerPos() {
-        if (client.player == null) return Vec3d.ZERO;
-        return client.player.getPos();
+    public static float getYaw() {
+        if (client.player == null) return 0.0F;
+        return client.player.getYaw(1.0F);
     }
 
-    public static float getPlayerYaw() {
-        if (client.player == null) return 0;
+    public static float getPitch() {
+        if (client.player == null) return 0.0F;
         return client.player.getPitch(1.0F);
-    }
-
-    public static float getPlayerPitch() {
-        if (client.player == null) return 0;
-        return client.player.getPitch();
-    }
-
-    public static int getPlayerHealth() {
-        if (client.player == null) return 0;
-        return (int) client.player.getHealth();
-    }
-
-    public static int getPlayerHunger() {
-        if (client.player == null) return 0;
-        return client.player.getHungerManager().getFoodLevel();
-    }
-
-    public static boolean isPlayerMoving() {
-        if (client.player == null) return false;
-        return client.player.getVelocity().length() > 0;
     }
 }
