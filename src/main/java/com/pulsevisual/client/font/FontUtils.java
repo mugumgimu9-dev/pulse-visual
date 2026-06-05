4aef18a4755d4ecc985655ee9454da4a4c452786
mugumgimu9-dev <mugumgimu9@gmail.com>
2026-06-05 09:54:13 +0300

    Update FontUtils.java

--- src/main/java/com/pulsevisual/client/font/FontUtils.java
+++ src/main/java/com/pulsevisual/client/font/FontUtils.java
@@ -2,23 +2,17 @@ package com.pulsevisual.client.font;
 
 import net.fabricmc.api.Environment;
 import net.fabricmc.api.EnvType;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.util.math.MatrixStack;
 
 @Environment(EnvType.CLIENT)
 public class FontUtils {
-    private static final MinecraftClient client = MinecraftClient.getInstance();
-    private static final TextRenderer renderer = client.textRenderer;
 
-    public static int getStringWidth(String text) {
-        return renderer.getWidth(text);
-    }
-
-    public static int getFontHeight() {
-        return renderer.fontHeight;
-    }
-
-    public static void drawString(String text, float x, float y, int color) {
-        renderer.draw(text, x, y, color);
+    public static void drawString(TextRenderer renderer, String text, float x, float y, int color) {
+        if (renderer == null || text == null) return;
+        
+        // В 1.16.5 обязательно передаем MatrixStack первым аргументом
+        MatrixStack matrixStack = new MatrixStack();
+        renderer.draw(matrixStack, text, x, y, color);
     }
 }
