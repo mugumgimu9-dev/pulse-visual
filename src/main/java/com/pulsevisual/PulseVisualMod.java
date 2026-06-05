35e5fc9107cd6b15d0e65850dc1c5c823fc05bb3
mugumgimu9-dev <mugumgimu9@gmail.com>
2026-06-05 15:47:31 +0300

    Update PulseVisualMod.java

--- src/main/java/com/pulsevisual/PulseVisualMod.java
+++ src/main/java/com/pulsevisual/PulseVisualMod.java
@@ -1,15 +1,12 @@
 package com.pulsevisual;
 
 import net.fabricmc.api.ModInitializer;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-public class PulseVisualMod implements ModInitializer {
-    public static final Logger LOGGER = LogManager.getLogger("PulseVisual");
-    public static final String MOD_ID = "pulse-visual";
-
+public class PulseVisual implements ModInitializer {
     @Override
     public void onInitialize() {
-        LOGGER.info("Initializing Pulse Visual Mod");
+        System.out.println("==================================================");
+        System.out.println("PULSE VISUAL MOD SUCCESSFULLY LOADED AND WORKING!");
+        System.out.println("==================================================");
     }
 }
