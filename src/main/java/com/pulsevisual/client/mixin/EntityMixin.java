c9799a869409edd2bdc4e74dbe0f6d39f36661ed
mugumgimu9-dev <mugumgimu9@gmail.com>
2026-06-09 22:11:39 +0300

    Create EntityMixin.java

---
+++ src/main/java/com/pulsevisual/client/mixin/EntityMixin.java
@@ -0,0 +1,25 @@
+package com.pulsevisual.client.mixin;
+
+import com.features.FriendManager;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Entity.class)
+public class EntityMixin {
+    @Inject(method = "isGlowing", at = @At("HEAD"), cancellable = true)
+    private void onIsGlowing(CallbackInfoReturnable<Boolean> cir) {
+        Entity entity = (Entity) (Object) this;
+        
+        // Если это игрок и он добавлен в друзья — включаем ему подсветку
+        if (entity instanceof PlayerEntity) {
+            String name = entity.getName().getString();
+            if (FriendManager.isFriend(name)) {
+                cir.setReturnValue(true);
+            }
+        }
+    }
+}
