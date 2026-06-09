62d8693ab8f54e42be98d8e08e2bb38e6fafc83c
mugumgimu9-dev <mugumgimu9@gmail.com>
2026-06-09 22:25:20 +0300

    Update FriendManager.java

--- src/main/java/com/features/FriendManager.java
+++ src/main/java/com/features/FriendManager.java
@@ -1,37 +1,20 @@
-package com.pulsevisual.client.mixin;
+package com.features;
 
-import com.features.FriendManager;
-import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.text.LiteralText;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import java.util.HashSet;
+import java.util.Set;
 
-@Mixin(ClientPlayerEntity.class)
-public class ClientPlayerEntityMixin {
-    @Inject(method = "sendChatMessage", at = @At("HEAD"), cancellable = true)
-    private void onSendMessage(String message, CallbackInfo ci) {
-        if (message.startsWith(".friends ")) {
-            ci.cancel();
-            
-            String[] args = message.split(" ");
-            if (args.length >= 3) {
-                String action = args[1];
-                String nickname = args[2];
-                
-                if (action.equalsIgnoreCase("add")) {
-                    FriendManager.addFriend(nickname);
-                    net.minecraft.client.MinecraftClient.getInstance().player.sendMessage(
-                        new LiteralText("§a[Pulse] " + nickname + " добавлен в список друзей!"), false
-                    );
-                } else if (action.equalsIgnoreCase("del") || action.equalsIgnoreCase("remove")) {
-                    FriendManager.removeFriend(nickname);
-                    net.minecraft.client.MinecraftClient.getInstance().player.sendMessage(
-                        new LiteralText("§c[Pulse] " + nickname + " удален из друзей."), false
-                    );
-                }
-            }
-        }
+public class FriendManager {
+    private static final Set<String> friends = new HashSet<>();
+
+    public static void addFriend(String name) {
+        friends.add(name.toLowerCase());
+    }
+
+    public static void removeFriend(String name) {
+        friends.remove(name.toLowerCase());
+    }
+
+    public static boolean isFriend(String name) {
+        return friends.contains(name.toLowerCase());
     }
 }
