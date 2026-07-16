442836256e5fa4ec8c67678bb0a4c35411534540
mugumgimu9-dev <mugumgimu9@gmail.com>
2026-07-16 17:41:18 +0300

    Update AdvancedESP.java

--- src/main/java/com/pulsevisual/client/gui/AdvancedESP.java
+++ src/main/java/com/pulsevisual/client/gui/AdvancedESP.java
@@ -1,4 +1,4 @@
-package com.pulsevisual.client;
+package com.pulsevisual.client.gui; // Пакет изменен на .gui, так как файл лежит там
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
@@ -6,10 +6,8 @@ import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.Box;
-import net.minecraft.util.math.Vec3d;
 import org.lwjgl.opengl.GL11;
 
 import java.awt.Color;
@@ -19,24 +17,12 @@ public class AdvancedESP {
 
     private final MinecraftClient client = MinecraftClient.getInstance();
 
-    // --- НАСТРОЙКИ МОДУЛЯ (Пока жестко прописаны, т.к. нет GUI) ---
     public boolean isEnabled = true;
-
-    // Режим подсветки: 0 = Отключен, 1 = Боксы (3D), 2 = Щейдеры (Аутлайн)
-    public int renderMode = 1;
-
-    // Режим выбора цвета: 0 = Статичный, 1 = По здоровью (Радуга), 2 = Радуга
-    public int colorMode = 0;
-
-    // Статичный цвет (если colorMode = 0)
+    public int renderMode = 1; // 1 = Боксы, 2 = Шейдеры
+    public int colorMode = 0;  // 0 = Статичный, 1 = По здоровью, 2 = Радуга
     public Color staticColor = Color.RED;
 
-    // Прозрачность (0-255)
-    public int espAlpha = 150;
-
-    // --- ИНИЦИАЛИЗАЦИЯ ---
     public void init() {
-        // Регистрируем рендер ивент
         WorldRenderEvents.LAST.register(this::onRenderWorld);
     }
 
@@ -46,59 +32,44 @@ public class AdvancedESP {
         List<? extends PlayerEntity> players = client.world.getPlayers();
         MatrixStack matrices = context.matrixStack();
         Camera camera = context.camera();
-        VertexConsumerProvider.Immediate consumers = client.getBufferBuilders().getEntityVertexConsumers();
 
         for (PlayerEntity player : players) {
-            if (player == client.player) continue; // Не подсвечиваем себя
-
-            // Проверяем дистанцию (например, 128 блоков)
+            if (player == client.player) continue;
             if (client.player.distanceTo(player) > 128) continue;
 
             Color drawColor = getESPColor(player);
 
-            // Обработка режимов рендера
             switch (renderMode) {
                 case 1 -> draw3DBoxESP(matrices, camera, player, drawColor);
-                case 2 -> applyShaderOutline(player, drawColor);
+                case 2 -> applyShaderOutline(player);
             }
         }
     }
 
-    // --- ВСПОМОГАТЕЛЬНЫЕ МЕТОДЫ РЕНДЕРА ---
-
-    // 1. Метод для 3D Боксов
-    private void draw3DBoxESP(MatrixStack matrices, Camera camera, Entity entity, Color color) {
+    private void draw3DBoxESP(MatrixStack matrices, Camera camera, PlayerEntity entity, Color color) {
         RenderSystem.enableBlend();
         RenderSystem.defaultBlendFunc();
         RenderSystem.disableTexture();
         RenderSystem.depthMask(false);
         GL11.glDisable(GL11.GL_DEPTH_TEST);
+        
+        // Переключаем рендер на дебаг-линии без использования VertexFormat.DrawMode
         RenderSystem.setShader(GameRenderer::getPositionColorShader);
 
         Tessellator tessellator = Tessellator.getInstance();
         BufferBuilder bufferBuilder = tessellator.getBuffer();
 
-        Vec3d cameraPos = camera.getPos();
-        double x = entity.getX() - cameraPos.x;
-        double y = entity.getY() - cameraPos.y;
-        double z = entity.getZ() - cameraPos.z;
+        double x = entity.getX() - camera.getPos().x;
+        double y = entity.getY() - camera.getPos().y;
+        double z = entity.getZ() - camera.getPos().z;
 
         Box box = entity.getBoundingBox();
         Box drawBox = new Box(x - (box.getXLength() / 2), y, z - (box.getZLength() / 2),
                                x + (box.getXLength() / 2), y + box.getYLength(), z + (box.getZLength() / 2));
 
-        bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
-
-        // Рисуем грани бокса (с прозрачностью)
-        int r = color.getRed(), g = color.getGreen(), b = color.getBlue(), a = espAlpha;
-        drawBox(bufferBuilder, drawBox, r, g, b, a);
-
-        tessellator.draw();
-
-        // Рисуем контур бокса
-        bufferBuilder.begin(VertexFormat.DrawMode.DEBUG_LINES, VertexFormats.POSITION_COLOR);
-        WorldRenderer.drawBox(matrices, bufferBuilder, drawBox, r/255f, g/255f, b/255f, a/255f);
-        tessellator.draw();
+        // Безопасный вызов отрисовки контура через встроенный WorldRenderer
+        // Он сам внутри себя открывает и закрывает буфер нужным образом
+        WorldRenderer.drawBox(matrices, bufferBuilder, drawBox, color.getRed()/255f, color.getGreen()/255f, color.getBlue()/255f, 0.6f);
 
         GL11.glEnable(GL11.GL_DEPTH_TEST);
         RenderSystem.depthMask(true);
@@ -106,77 +77,22 @@ public class AdvancedESP {
         RenderSystem.disableBlend();
     }
 
-    // Вспомогательный метод для рисования заполненного бокса
-    private void drawBox(BufferBuilder bufferBuilder, Box box, int r, int g, int b, int a) {
-        float f1 = (float)box.minX, f2 = (float)box.minY, f3 = (float)box.minZ;
-        float f4 = (float)box.maxX, f5 = (float)box.maxY, f6 = (float)box.maxZ;
-
-        // Down
-        bufferBuilder.vertex(f1, f2, f3).color(r, g, b, a).next();
-        bufferBuilder.vertex(f4, f2, f3).color(r, g, b, a).next();
-        bufferBuilder.vertex(f4, f2, f6).color(r, g, b, a).next();
-        bufferBuilder.vertex(f1, f2, f6).color(r, g, b, a).next();
-
-        // Up
-        bufferBuilder.vertex(f1, f5, f3).color(r, g, b, a).next();
-        bufferBuilder.vertex(f1, f5, f6).color(r, g, b, a).next();
-        bufferBuilder.vertex(f4, f5, f6).color(r, g, b, a).next();
-        bufferBuilder.vertex(f4, f5, f3).color(r, g, b, a).next();
-
-        // North
-        bufferBuilder.vertex(f1, f2, f3).color(r, g, b, a).next();
-        bufferBuilder.vertex(f1, f5, f3).color(r, g, b, a).next();
-        bufferBuilder.vertex(f4, f5, f3).color(r, g, b, a).next();
-        bufferBuilder.vertex(f4, f2, f3).color(r, g, b, a).next();
-
-        // South
-        bufferBuilder.vertex(f1, f2, f6).color(r, g, b, a).next();
-        bufferBuilder.vertex(f4, f2, f6).color(r, g, b, a).next();
-        bufferBuilder.vertex(f4, f5, f6).color(r, g, b, a).next();
-        bufferBuilder.vertex(f1, f5, f6).color(r, g, b, a).next();
-
-        // West
-        bufferBuilder.vertex(f1, f2, f3).color(r, g, b, a).next();
-        bufferBuilder.vertex(f1, f2, f6).color(r, g, b, a).next();
-        bufferBuilder.vertex(f1, f5, f6).color(r, g, b, a).next();
-        bufferBuilder.vertex(f1, f5, f3).color(r, g, b, a).next();
-
-        // East
-        bufferBuilder.vertex(f4, f2, f3).color(r, g, b, a).next();
-        bufferBuilder.vertex(f4, f5, f3).color(r, g, b, a).next();
-        bufferBuilder.vertex(f4, f5, f6).color(r, g, b, a).next();
-        bufferBuilder.vertex(f4, f2, f6).color(r, g, b, a).next();
-    }
-
-    // 2. Метод для Шейдеров (Аутлайна)
-    // ВАЖНО: Ванильный Fabric аутлайн работает нестабильно без Mixin'ов.
-    // Это базовая реализация черезGlowing эффект.
-    private void applyShaderOutline(PlayerEntity player, Color color) {
-        // Мы просто включаем свечение ванильным способом
-        // Чтобы менять ЦВЕТ свечения, нужны продвинутые миксины в RenderLayers.
-        // Сейчас он будет просто ванильно-белым/командным.
+    private void applyShaderOutline(PlayerEntity player) {
         player.setGlowing(true);
     }
-    // Метод для выключения аутлайна (нужно вызывать, когда модуль выключается)
-    public void disableShaders() {
-        if (client.world != null) {
-            client.world.getPlayers().forEach(p -> p.setGlowing(false));
-        }
-    }
 
-    // --- ЛОГИКА ЦВЕТОВ ---
     private Color getESPColor(PlayerEntity player) {
         switch (colorMode) {
-            case 1: // По здоровью (От зеленого к красному)
+            case 1:
                 float health = player.getHealth();
                 float maxHealth = player.getMaxHealth();
-                float hue = Math.max(0.0F, Math.min(health / maxHealth, 1.0F)) / 3.0F; // 0.0-0.33 (красный-зеленый)
+                float hue = Math.max(0.0F, Math.min(health / maxHealth, 1.0F)) / 3.0F;
                 return Color.getHSBColor(hue, 1.0F, 1.0F);
-            case 2: // Радуга
+            case 2:
                 float rainbowHue = (System.currentTimeMillis() % 3000) / 3000f;
                 return Color.getHSBColor(rainbowHue, 1.0F, 1.0F);
-            default: // Статичный
+            default:
                 return staticColor;
         }
     }
-                              }
+}
